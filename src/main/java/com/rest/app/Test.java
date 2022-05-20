ec5ad34ff8a71d3b1581d8ba20722d4e3cd0b250
rudragoudamp <rudragouda90@gmail.com>
2022-05-20 10:04:52 +0530

    Committing Test changes

---
+++ src/main/java/com/rest/app/Test.java
@@ -0,0 +1,14 @@
+package com.rest.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String hungry = "Yes";
+		if("Yes".equalsIgnoreCase(hungry)) {
+			System.out.println("Eat Something......");
+		}else {
+			System.out.println("Do Work......");
+		}
+	}
+}
